3d700ab57e37971bc652fb5b255594c34580dd30
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2025-08-12 17:58:08 +0530

    first program

--- src/test/java/com/thetestingacademy/APITest001.java
+++ src/test/java/com/thetestingacademy/APITest001.java
@@ -4,16 +4,7 @@ import io.restassured.RestAssured;
 
 public class APITest001 {
     public static void main(String[] args) {
-        RestAssured
-                .given()
-                    .baseUri("https://restful-booker.herokuapp.com")
-                    .basePath("/booking/1")
-                .when()
-                    .get()
-                .then()
-                    .log().all()
-                    .statusCode(200);
-
+        System.out.println("Hello Rest Assured.");
 
     }
 }
---
+++ src/test/java/com/thetestingacademy/APITest003NonDesignPattern.java
@@ -0,0 +1,23 @@
+package com.thetestingacademy;
+
+public class APITest003NonDesignPattern {
+    public void step1() {
+        System.out.println("step 1 ");
+    }
+
+    public void step2() {
+        System.out.println("step 2 ");
+    }
+
+    public void step3(String s1) {
+        System.out.println("step 3 ");
+    }
+
+
+    public static void main(String[] args) {
+        APITest003NonDesignPattern ndp = new APITest003NonDesignPattern();
+        ndp.step1();
+        ndp.step2();
+        ndp.step3("Suraj");
+    }
+}

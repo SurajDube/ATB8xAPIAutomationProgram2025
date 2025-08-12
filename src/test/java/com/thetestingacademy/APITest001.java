35bafc3f878234bc85139077a1ada53d921c29c0
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2025-08-12 17:45:54 +0530

    first program

---
+++ src/test/java/com/thetestingacademy/APITest001.java
@@ -0,0 +1,19 @@
+package com.thetestingacademy;
+
+import io.restassured.RestAssured;
+
+public class APITest001 {
+    public static void main(String[] args) {
+        RestAssured
+                .given()
+                    .baseUri("https://restful-booker.herokuapp.com")
+                    .basePath("/booking/1")
+                .when()
+                    .get()
+                .then()
+                    .log().all()
+                    .statusCode(200);
+
+
+    }
+}
